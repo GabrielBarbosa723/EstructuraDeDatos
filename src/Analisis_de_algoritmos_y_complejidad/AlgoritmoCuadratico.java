ca4968c7624ec2d9f33382451a89fd2b793bcb68
Gabriel <gabriel.barbosa7237@gmail.com>
2026-09-05 17:18:06 -0500

    Agrego algoritmo cuadratico

---
+++ src/Analisis_de_algoritmos_y_complejidad/AlgoritmoCuadratico.java
@@ -0,0 +1,27 @@
+
+package Analisis_de_algoritmos_y_complejidad;
+
+
+public class AlgoritmoCuadratico {
+    
+    public static void main (String[] args){
+        
+        int n = 5;
+        int contador = 0;
+        
+        // algortimo O(n²)
+        
+        for (int i= 0; i<n ; i++){
+            
+            for (int j=0; j<n; j++){
+                
+                System.out.println(i +" " + j);
+                contador ++;
+            }
+        }
+        
+        System.out.println("total de ejecuciones " + contador);
+        
+    }
+    
+}
