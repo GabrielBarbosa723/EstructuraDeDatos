868a1d7cf626bfce5f6c3de26ea7cd47bfee54b0
Gabriel <gabriel.barbosa7237@gmail.com>
2026-09-06 11:45:30 -0500

    Agrego algoritmo logaritmico~

---
+++ src/Analisis_de_algoritmos_y_complejidad/AlgoritmoLogaritmico.java
@@ -0,0 +1,36 @@
+
+package Analisis_de_algoritmos_y_complejidad;
+
+
+public class AlgoritmoLogaritmico {
+    
+    public static void main (String []args){
+        
+        
+        int n = 129;
+        int contador = 0;
+        
+        int i = 1;
+        
+        while (i<n){
+            
+            System.out.println("Valor: "+i);
+            
+            i = i*2;
+            contador++;
+            
+            
+            
+            
+        }
+        
+        System.out.println("Total de ejecuciones: "+ contador);
+        
+        
+    }
+    
+    
+    
+    
+    
+}
